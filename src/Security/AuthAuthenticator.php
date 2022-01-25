ee84a4aac69e0433e70c6bf78a053d13c2d85df2
diego_garcia <diego_garcia@doctaforum.com>
2022-01-25 09:15:17 +0100

    admin home created

--- src/Security/AuthAuthenticator.php
+++ src/Security/AuthAuthenticator.php
@@ -91,14 +91,14 @@ class AuthAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        echo "Logueado correctamente";
+        return new RedirectResponse("home");
 
-        if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
-        }
+        // if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
+        //     return new RedirectResponse($targetPath);
+        // }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
